e4d2900e6c8a125b226ed79e117a806f12f90b86
Balor <polpoillopi@gmail.com>
2024-03-10 14:57:49 +0700

    modify to shift <<

--- src/Lab3.2/Lab3.2.ino
+++ src/Lab3.2/Lab3.2.ino
@@ -11,10 +11,10 @@ int main(void) {
 
     uint32_t sctext[] = { 0x7c000000, 0x7e003800, 0x63001800, 0x633c1800, 0x7e661800, 0x7c661800, 0x60661800, 0x603c3c00 };
 Serial.println("fjdk");
-    for (int j=0; j < 32; j++) {
+    for (int j=32; j > 0; j--) {
       Serial.println("no3");
       for(int k = 0;k<8;k++)
-      temp[k] = 0xFF & sctext[k] >> j;
+      temp[k] = 0xFF & sctext[k]>>j;
       for (int i = 0; i < 8; i++) {
         max7219_wr(i + 1, (temp[i]));
         Serial.println("fjdk32");
